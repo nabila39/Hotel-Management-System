ecef51e285ab425a9b5d99dbe275150a2b54c487
nabila39 <nabilaothman2468@gmail.com>
2024-06-16 15:57:41 +0300

    initialize the project

---
+++ src/main/java/com/example/FinalWebProject/FinalWebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.FinalWebProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalWebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalWebProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FinalWebProject/FinalWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FinalWebProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalWebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
